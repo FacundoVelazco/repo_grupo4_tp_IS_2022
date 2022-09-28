4ce26f6b0f55dbb87a90098edb3464aaff113447
Facundo Velazco <elfacu@MBP-de-Facundo.fibertel.com.ar>
2022-09-28 10:54:48 -0300

    2.a modificacion realizada

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const { Console } = require("./console");
 
 const console = new Console();
 
-console.writeln("Hello world")
+console.writeln("Hello world");
+
+console.writeln("Un cambio -- Ejercicio 2.a");
